7df145208a49409d709f19dae3dbb84707fdc117
Derek Hammer <hammer@stsci.edu>
2013-09-05 14:54:42 -0400

    Updated to include same options as run_tweakreg_flt.

--- run_tweakreg_drz.py
+++ run_tweakreg_drz.py
@@ -2,7 +2,7 @@
 
 '''
 ABOUT:
-This program runs tweakreg on a list of _fl? images or drizzled-to-drizzled images using external catalogs to compare object centroids.
+This program runs tweakreg on a list of _fl? images
 
 DEPENDS:
 Python 2.5.4
@@ -16,7 +16,7 @@ July 2013: Original script (v0.1).
 FUTURE IMPROVEMENTS:
 
 USE:
-python run_tweakreg.py
+python run_tweakreg_flt.py
 '''
 
 __author__='D.M. HAMMER'
@@ -31,19 +31,31 @@ from drizzlepac import tweakreg
 if __name__=='__main__':
     # Parse input parameters
     parser = argparse.ArgumentParser(description='Run tweakreg on input images using custom source catalogs.')
-    parser.add_argument('-im', '--images',default='*fl?.fits', type=str, help='Input flt or drz image(s). \
+    parser.add_argument('-im', '--images',default='*fl?.fits', type=str, help='Input image file(s). \
 				 Default is all _fl? images in current directory.')
     parser.add_argument('-cf', '--catfile',default='catfile.sex', type=str, help='Input name of catfile. \
 				 Default is catfile.sex.')
     parser.add_argument('-log', '--logfile',default='tweakreg.log', type=str, help='Input name of tweakreg log file. \
 				 Default is tweakreg.log')
-    parser.add_argument('-rim', '--refim',default='', type=str, help='Input refererence image. \
+    parser.add_argument('-rim', '--refim',default='', type=str, help='Input reference image. \
                                  There is no default - must be entered.')
     parser.add_argument('-rcat', '--refcat',default='', type=str, help='Input image file(s). \
                                  There is no default - must be entered.')
+    parser.add_argument('-xc', '--xcol',default=8, type=int, help='Input catalog column that corresponds to x. \
+                                 Default = SExtractor XWIN column.')
+    parser.add_argument('-yc', '--ycol',default=9, type=int, help='Input catalog column that corresponds to y. \
+                                 Default = SExtractor YWIN column.')
+    parser.add_argument('-rxc', '--refxcol',default=8, type=int, help='Input (ref)catalog column that corresponds to x. \
+                                 Default = SExtractor XWIN column.')
+    parser.add_argument('-ryc', '--refycol',default=9, type=int, help='Input (ref)catalog column that corresponds to y. \
+                                 Default = SExtractor YWIN column.')
     options = parser.parse_args()
     logfile = options.logfile
     catfilename = options.catfile
+    xcol = options.xcol
+    ycol = options.ycol
+    rxcol = options.refxcol
+    rycol = options.refycol
 
     # -- parse input to hold image and reference info
     im = glob.glob(options.images)
@@ -86,10 +98,10 @@ if __name__=='__main__':
     # -- run tweakreg (SExtractor catalog: XWIN cols 8/9; XORG cols 2/3)
     iraf.unlearn('tweakreg')
     if USE_REF:
-        tweakreg.TweakReg('@imlist.dat',catfile=catfilename,xcol=8,ycol=9,refimage=irefim,refcat=irefcat,refxyunits='pixels',refxcol=8,refycol=9, runfile=logfile, \
+        tweakreg.TweakReg('@imlist.dat',catfile=catfilename,xcol=xcol,ycol=ycol,refimage=irefim,refcat=irefcat,refxyunits='pixels',refxcol=rxcol,refycol=rycol, runfile=logfile, \
 			conv_width=conv_wid,searchrad=3.0,updatehdr=True,nclip=7,shiftfile=True,outshifts='shift.dat', see2dplot=False,residplot='No plot')
     else:
-        tweakreg.TweakReg('@imlist.dat',catfile=catfilename,xcol=8,ycol=9,conv_width=conv_wid,searchrad=3.0, runfile=logfile, \
+        tweakreg.TweakReg('@imlist.dat',catfile=catfilename,xcol=xcol,ycol=ycol,conv_width=conv_wid,searchrad=3.0, runfile=logfile, \
 			updatehdr=True,nclip=7,shiftfile=True,outshifts='shift.dat',see2dplot=False,residplot='No plot')
 
 
